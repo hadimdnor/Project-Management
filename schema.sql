b92cc8da2f6a5920ed447e3255503d6b6b249731
Nurul Aida <NurulAida@NurulAida-iMac.local>
2021-01-06 23:24:36 +0800

    insert new task

--- schema.sql
+++ schema.sql
@@ -7,9 +7,16 @@ CREATE TABLE tasks(id SERIAL PRIMARY KEY, task_name TEXT, due_date TEXT, staff_i
 
 INSERT INTO tasks(task_name, due_date, details) VALUES('Task 1 - Tic Tac Toe', '2021 January 10', 'Let''s start out with something fun - a game! Everyone will get a chance to be creative, and work through some really tough programming challenges – Let''s start out with something fun - a game! We''ll be making tic tac toe (knots and crosses). What a nice surprise! But it''s up to you to come up with a fun and interesting approach to this classic game. You will be working individually for this project, but we''ll be guiding you along the process and helping you as you go. Show us what you''ve got!');
 
-INSERT INTO tasks (task_name, due_date, details) VALUES ('Task 2 - CRUD Apps', '2021 February 01', 'Make a web apps that requires you to use create, read, update and delete apps.');
+INSERT INTO tasks(task_name, due_date, details) VALUES ('Task 2 - CRUD Apps', '2021 February 01', 'Make a web apps that requires you to use create, read, update and delete apps.');
+
+INSERT INTO tasks(task_name, due_date, details) VALUES('Task 3 - Front End', '2021 January 20', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde, optio temporibus, labore earum est aspernatur possimus itaque quidem porro molestiae sapiente maxime asperiores! Reiciendis nam dolor adipisci suscipit facilis!')
+
+INSERT INTO tasks(task_name, due_date, details) VALUES('Task 4 - Server Part', '2021 January 20', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, laudantium reiciendis. Deserunt consequuntur maxime sunt aspernatur nemo tempore officia, illum neque. In deserunt omnis architecto dolore iure reiciendis distinctio pariatur!')
+
+INSERT INTO tasks(task_name, due_date, details) VALUES('Task 5 - Stylish CSS', '2021 January 19', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum mollitia nulla aperiam cum labore consequuntur earum sapiente, obcaecati quia cumque libero iure quos, voluptate officia velit expedita eligendi suscipit voluptatibus?')
+
+INSERT INTO tasks (task_name, due_date, details) VALUES ('Task 6 - HUHUHU Apps', '2021 February 01', 'Make a web apps that requires you to use create, read, update and delete apps.');
 
 SELECT * FROM users;
 SELECT * FROM tasks;
 
-INSERT INTO tasks (task_name, due_date, details) VALUES ('Task 3 - HUHUHU Apps', '2021 February 01', 'Make a web apps that requires you to use create, read, update and delete apps.');
